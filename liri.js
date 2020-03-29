820c9f9355487ba505cc563ab3d732f7ee4be799
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-03-29 14:30:29 -0700

    Retrieved Conert information, working on the date format for the events

--- liri.js
+++ liri.js
@@ -1,6 +1,83 @@
 require("dotenv").config();
+
+const axios = require("axios");
+const moment = require("moment");
+
 var keys = require("./keys.js");
+
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+var concertAPI = keys.concert;
+
+// Outputs the right way to use the program
+function usage() {
+  console.group("Error! Enter any of the following commands:");
+  console.log("node liri.js concert-this '<artist/band name>");
+  console.log("node liri.js spotify-this-song '<song name>'");
+  console.log("node liri.js movie-this '<movie name>'");
+  console.log("node liri.js do-what-it-says");
+  console.groupEnd();
+}
+
+function findConcerts(arg) {
+  var queryURL =
+    "https://rest.bandsintown.com/artists/celine+dion/events?app_id=" +
+    concertAPI;
+  axios
+    .get(queryURL)
+    .then(function(response) {
+      console.log("Fetched Concerts: ");
+      var concerts = [];
+      response.data.forEach((element) => {
+        var m = moment(element.datetime, "MM/DD/YYYY");
+        concerts.push({
+          Venue: element.venue.name,
+          Location:
+            element.venue.city +
+            ", " +
+            element.venue.region +
+            ", " +
+            element.venue.country,
+          Date: m.format()
+        });
+      });
+      console.table(concerts);
+    })
+    .catch(function(error) {
+      console.log("ERROR" + error);
+    });
+}
+// Executes the entered command
+function executeCommand(args) {
+  switch (args[0]) {
+    case "concert-this":
+      console.log("Finding Concert");
+      findConcerts(args[1]);
+      break;
+    case "spotify-this-song":
+      console.log("Spotifying");
+      break;
+    case "movie-this":
+      console.log("Finding Moive");
+      break;
+    case "do-what-it-says":
+      console.log("Reading File");
+      break;
+    default:
+      console.log("Command <" + args[0] + "> not found");
+      usage();
+  }
+}
+
+if (process.argv.length <= 2) {
+  return usage();
+}
+
+var arguments = process.argv.slice(2);
+console.log("Arguements: " + arguments);
+
+executeCommand(arguments);
 
 //concert-this
 
